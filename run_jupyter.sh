2448d22c34877dda24708526bfcf2e8ce0698988
dadidange <david.gmelin@student.uni-luebeck.de>
2021-02-10 12:48:48 +0100

    cleanup for pull request

--- run_jupyter.sh
+++
@@ -1,8 +0,0 @@
-docker run \
-    --gpus all \
-    --name dl_jupyter \
-    --rm -p 8888:8888 -v $(pwd):/workingdir -v /nfshome/gmelin/Documents/repos/dl_bio_example:/data \
-    dl_workingdir jupyter notebook . \
-    --ip=0.0.0.0 \
-    --no-browser \
-    --allow-root 
