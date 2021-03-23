03f585ff7306e57dc011f0a29ee3a6f6fb13c307
dadidange <david.gmelin@student.uni-luebeck.de>
2021-03-23 15:50:57 +0100

    added conversion to uint8

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import json
 from os.path import join
-
+import numpy as np
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 
-def log_tensorboard(workdir, tb_out, dataloaders, numBatches=1, model=None):
+def log_tensorboard(workdir, tb_out, dataloaders, numBatches=1, model=None, to_uint_8=True):
 	""" write data for visualization by tensorboard
 
 	Start TensorBoard with 
@@ -23,10 +23,13 @@ def log_tensorboard(workdir, tb_out, dataloaders, numBatches=1, model=None):
 		number of image batches to display, by default 1
 	model : pytorch model, optional
 		the model used in trainig, will be displayed as graph in TensorBoard, by default None
+	to_uint_8 : bool, optional
+		convert images to uint8 to display them, by default True
 	"""	
 
 	with open(join(workdir, 'log.json'), 'r') as file:
 		log = json.load(file)
+
 
 	try:	
 		tb_writer = SummaryWriter(tb_out) 
@@ -39,7 +42,13 @@ def log_tensorboard(workdir, tb_out, dataloaders, numBatches=1, model=None):
 				di = iter(dl)
 				for i in range(numBatches):
 					images, _ = di.next()
-					grid = torchvision.utils.make_grid(images)		
+					grid = torchvision.utils.make_grid(images)	
+					if to_uint_8:
+						grid -= grid.min()
+						grid /= grid.max()
+						grid *= 255
+						grid = np.array(grid.detach().cpu()).astype('uint8')
+
 					tb_writer.add_image(f"{key}_batch{i}", grid)
 			
 		# load model if needed
@@ -49,8 +58,12 @@ def log_tensorboard(workdir, tb_out, dataloaders, numBatches=1, model=None):
 		print(f'finished writing to {tb_out}')	
 	except Exception as e:
 		print("Exception thrown while logging to SummaryWriter:")
-		print(e.__class__, e.__traceback__)
+		print(e.__class__)
+		print(e)
 	finally:
 		tb_writer.flush()
 		tb_writer.close()
+
+
+
 
